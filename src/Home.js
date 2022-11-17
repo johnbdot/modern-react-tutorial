84a45cbc44a6c5c94461490bc3aed7bb64897784
John Bouakhasith <johnbeedot@gmail.com>
2022-11-17 11:48:33 -0500

    props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -11,15 +12,11 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const hello = "hello!!";
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" anotherTitle={hello} />
     </div>
   );
 };
